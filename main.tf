4213c31fa053a2cee4e0a7d1ce474c4d7402a5ca
Eze Onuoha <onuohaeze@gmail.com>
2020-10-08 13:06:14 -0400

    Initial commit

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+    version = "~> 2.0"
+    region = "us-east-2"
+}
+
+resource "aws_instance" "example" {
+
+    ami			= "ami-0c55b159cbfafe1f0"
+    instance_type 	= "t2.micro"
+
+    tags = {
+      Name = "terraform-example"
+    }
+}
+    
