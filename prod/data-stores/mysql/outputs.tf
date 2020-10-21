ee816e29fd7856ea79b76e566b2c3ecb41807eff
Eze Onuoha <onuohaeze@gmail.com>
2020-10-21 18:56:16 -0400

    Streamlined files

--- prod/data-stores/mysql/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "address" {
-    value	= aws_db_instance.prod.address
-    description = "Connect to the Database at this endpoint"
-}
-
-output "port" {
-    value	= aws_db_instance.prod.port
-    description = "The port the Database is listening on"
-}
