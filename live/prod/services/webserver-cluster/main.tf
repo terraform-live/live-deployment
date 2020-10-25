155612e35c9cd017abba2a72bfbf0136281b5b97
Eze Onuoha <onuohaeze@gmail.com>
2020-10-25 15:04:30 -0400

    Enable autoscaling schedule

--- live/prod/services/webserver-cluster/main.tf
+++ live/prod/services/webserver-cluster/main.tf
@@ -16,6 +16,7 @@ module "webserver_cluster" {
   instance_type = "t2.micro"
   min_size = 2
   max_size = 4
+  enable_autoscaling      = true
 
   custom_tags = {
     Owner     = "delta-team"
@@ -23,22 +24,4 @@ module "webserver_cluster" {
   }
 }
 
-resource "aws_autoscaling_schedule" "scale_out_during_business_hours" {
-  
-  scheduled_action_name = "scale-out-during-business-hours"
-  min_size = 4
-  max_size = 10
-  desired_capacity = 10
-  recurrence = "00 11 * * *"
-  autoscaling_group_name = module.webserver_cluster.asg_name
-}
-resource "aws_autoscaling_schedule" "scale_in-at-night" {
-  
-  scheduled_action_name = "scale-in-at-night"
-  min_size = 2
-  max_size = 10
-  desired_capacity = 2
-  recurrence = "0 17 * * *"
-  autoscaling_group_name = module.webserver_cluster.asg_name
-}
 
